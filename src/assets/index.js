0c32bfc299303a424559bdd595e6fe4e5366bbec
huntermacias <supercodersf@gmail.com>
2023-01-17 08:44:12 -0800

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import homeHero from "./home_hero.png";
+import homeCards from "./home_cards.png";
+import feature from "./feature.png";
+import expo from "./expo.png";
+import expo02 from "./expo02.png";
+import javascript from "./javascript.png";
+import react from "./react.png";
+import scene from "./scene.png";
+import mockup from "./mockup.png";
+// import banner from "./banner"
+// import banner02 from "./banner02"
+// import banner03 from "./banner03"
+// import banner04 from "./banner04"
+
+export default {
+  homeHero,
+  homeCards,
+  feature,
+  expo,
+  expo02,
+  javascript,
+  react,
+  scene,
+  mockup,
+  // banner,
+  // banner02,
+  // banner03,
+  // banner04
+};
