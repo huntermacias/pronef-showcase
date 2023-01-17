cca0143788bc67228d143f77be9c6c1159be00a2
huntermacias <supercodersf@gmail.com>
2023-01-17 08:53:12 -0800

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Features from './Features';
 import Download from './Download';
 import SectionWrapper from './SectionWrapper';
-import Button from './Button';
 
-export { Features, Download, SectionWrapper, Button };
+export { Features, Download, SectionWrapper };
